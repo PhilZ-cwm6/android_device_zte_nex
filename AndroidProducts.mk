338107aa0926b6be7f93a16878ca6e19ad43c274
Czar Dixon <rbheromax@gmail.com>
2014-01-05 02:42:38 -0500

    changing files around in ramdisk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,5 +2,17 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device_nex.mk
 
 PRODUCT_COPY_FILES := \
-    $(LOCAL_DIR)/rootdir/fstab.qcom:root/fstab.qcom
-# Files from root for ramdisk, just didn't add that yet
+    $(LOCAL_DIR)/ramdisk/default.prop:root/default.prop \
+    $(LOCAL_DIR)/ramdisk/file_contexts:root/file_contexts \
+    $(LOCAL_DIR)/ramdisk/fstab.qcom:root/fstab.qcom \
+    $(LOCAL_DIR)/ramdisk/init:root/init \
+    $(LOCAL_DIR)/ramdisk/init.cm.rc:root/init.cm.rc \
+    $(LOCAL_DIR)/ramdisk/init.environ.rc:root/init.environ.rc \
+    $(LOCAL_DIR)/ramdisk/init.rc:root/init.rc \
+    $(LOCAL_DIR)/ramdisk/init.superuser.rc:root/init.superuser.rc \
+    $(LOCAL_DIR)/ramdisk/init.trace.rc:root/init.trace.rc \
+    $(LOCAL_DIR)/ramdisk/init.usb.rc:root/init.usb.rc \
+    $(LOCAL_DIR)/ramdisk/property_contexts:root/property_contexts \
+    $(LOCAL_DIR)/ramdisk/seapp_contexts:root/seapp_contexts \
+    $(LOCAL_DIR)/ramdisk/sepolicy:root/sepolicy
+
